52c879a1c333e9d12e783167a498c598dbb72975
Ivan Pik <ivan.pik@basekit.com>
2015-06-26 13:52:22 +0100

    Prevent navigation from opening in the edit more

--- js/custom.js
+++ js/custom.js
@@ -262,4 +262,39 @@ $(document).ready(function() {
     };
 
 
+
+
+    // Toggles class open to display/hide the whole navigation
+$( ".widget__extendednavigation" ).addClass( "closed" );
+
+
+if(publishedmode==true) {
+    $( ".extendednavigation__navigation-toggle" ).click(function() {
+        $( ".widget__extendednavigation" ).toggleClass( "open" );
+        $( "body" ).toggleClass( "navigation--open" );
+    });
+
+
+
+    $('.navigation-body').click(function(e) {
+        if (e.target == this) {
+            $( ".widget__extendednavigation" ).removeClass( "open" );
+            $( "body" ).removeClass( "navigation--open" );
+        }
+    });
+
+
+
+
+
+    // Togglse class open to display/hide folders
+
+    $( ".navigation-item.folder .item-name--parent" ).each(function() {
+        $( this ).click(function() {
+                $( this ).parent( ".navigation-item.folder" ).toggleClass( "open" );
+        });
+    });
+}
+
+
 });
